7f68fcd0935b7d8e17d713b59940f99cd2b770f4
yliangsiew <yliangsiew@blizzard.com>
2017-06-29 14:11:41 -0700

    added support for imenu and fixed transient mark

--- lightweight_config.el
+++ lightweight_config.el
@@ -34,6 +34,61 @@
 ; Indepedent space/hypen matching for ido-mode
 (require 'ido-complete-space-or-hyphen)
 
+; Allow for ido-mode to be used with imenu for looking through source file functions/members
+; (require 'imenu)
+; (require 'idomenu)
+; (autoload 'idomenu "idomenu" nil t)
+
+(defun ido-goto-symbol (&optional symbol-list)
+      "Refresh imenu and jump to a place in the buffer using Ido."
+      (interactive)
+      (unless (featurep 'imenu)
+        (require 'imenu nil t))
+      (cond
+       ((not symbol-list)
+        (let ((ido-mode ido-mode)
+              (ido-enable-flex-matching
+               (if (boundp 'ido-enable-flex-matching)
+                   ido-enable-flex-matching t))
+              name-and-pos symbol-names position)
+          (unless ido-mode
+            (ido-mode 1)
+            (setq ido-enable-flex-matching t))
+          (while (progn
+                   (imenu--cleanup)
+                   (setq imenu--index-alist nil)
+                   (ido-goto-symbol (imenu--make-index-alist))
+                   (setq selected-symbol
+                         (ido-completing-read "Symbol? " symbol-names))
+                   (string= (car imenu--rescan-item) selected-symbol)))
+          (unless (and (boundp 'mark-active) mark-active)
+            (push-mark nil t nil))
+          (setq position (cdr (assoc selected-symbol name-and-pos)))
+          (cond
+           ((overlayp position)
+            (goto-char (overlay-start position)))
+           (t
+            (goto-char position)))))
+       ((listp symbol-list)
+        (dolist (symbol symbol-list)
+          (let (name position)
+            (cond
+             ((and (listp symbol) (imenu--subalist-p symbol))
+              (ido-goto-symbol symbol))
+             ((listp symbol)
+              (setq name (car symbol))
+              (setq position (cdr symbol)))
+             ((stringp symbol)
+              (setq name symbol)
+              (setq position
+                    (get-text-property 1 'org-imenu-marker symbol))))
+            (unless (or (null position) (null name)
+                        (string= (car imenu--rescan-item) name))
+              (add-to-list 'symbol-names name)
+              (add-to-list 'name-and-pos (cons name position))))))))    
+
+(global-set-key (kbd "C-c h i") 'ido-goto-symbol) ; or any key you see fit
+
 ; Undo functionality improved
 (require 'undo-tree)
 (global-undo-tree-mode)
@@ -56,7 +111,6 @@
 (setq lightweight-win32 (not (or lightweight-aquamacs lightweight-linux)))
 
 (global-hl-line-mode 1)
-(set-face-background 'hl-line "midnight blue")
 
 ; Start maximized
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
@@ -336,15 +390,6 @@
 (define-key global-map [pgdown] 'backward-page)
 (define-key global-map [C-next] 'scroll-other-window)
 (define-key global-map [C-prior] 'scroll-other-window-down)
-
-; ALT-alternatives
-(defadvice set-mark-command (after no-bloody-t-m-m activate)
-  "Prevent consecutive marks activating bloody `transient-mark-mode'."
-  (if transient-mark-mode (setq transient-mark-mode nil)))
-
-(defadvice mouse-set-region-1 (after no-bloody-t-m-m activate)
-  "Prevent mouse commands activating bloody `transient-mark-mode'."
-  (if transient-mark-mode (setq transient-mark-mode nil))) 
 
 (defun append-as-kill ()
   "Performs copy-region-as-kill as an append."
