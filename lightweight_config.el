d8eb9090d4eb42e881655b412cb63884a61a19c5
Siew Yi Liang <admin@sonictk.com>
2017-06-29 00:30:55 -0700

    adding modified version of casey muratori's lightweight emacs config

---
+++ lightweight_config.el
@@ -0,0 +1,400 @@
+; Stop Emacs from losing undo information by
+; setting very high limits for undo buffers
+(setq undo-limit 20000000)
+(setq undo-strong-limit 40000000)
+
+; Determine the underlying operating system
+(setq lightweight-aquamacs (featurep 'aquamacs))
+(setq lightweight-linux (featurep 'x))
+(setq lightweight-win32 (not (or lightweight-aquamacs lightweight-linux)))
+
+(global-hl-line-mode 1)
+(set-face-background 'hl-line "midnight blue")
+
+(setq compilation-directory-locked nil)
+(scroll-bar-mode -1)
+(setq shift-select-mode nil)
+(setq enable-local-variables nil)
+(setq lightweight-font "outline-DejaVu Sans Mono")
+
+(when lightweight-win32 
+  (setq lightweight-font "outline-Liberation Mono")
+)
+
+(when lightweight-aquamacs 
+  (cua-mode 0) 
+  (osx-key-mode 0)
+  (tabbar-mode 0)
+  (setq mac-command-modifier 'meta)
+  (setq x-select-enable-clipboard t)
+  (setq aquamacs-save-options-on-quit 0)
+  (setq special-display-regexps nil)
+  (setq special-display-buffer-names nil)
+  (define-key function-key-map [return] [13])
+  (setq mac-command-key-is-meta t)
+  (scroll-bar-mode nil)
+  (setq mac-pass-command-to-system nil)
+)
+
+(when lightweight-linux
+  (display-battery-mode 1)
+)
+
+; Turn off the toolbar
+(tool-bar-mode 0)
+
+(load-library "view")
+(require 'cc-mode)
+(require 'ido)
+(require 'compile)
+(ido-mode t)
+
+(defun lightweight-ediff-setup-windows (buffer-A buffer-B buffer-C control-buffer)
+  (ediff-setup-windows-plain buffer-A buffer-B buffer-C control-buffer)
+)
+(setq ediff-window-setup-function 'lightweight-ediff-setup-windows)
+(setq ediff-split-window-function 'split-window-horizontally)
+
+; Turn off the bell on Mac OS X
+(defun nil-bell ())
+(setq ring-bell-function 'nil-bell)
+
+; Setup my compilation mode
+(defun lightweight-compilation-hook ()
+  (make-local-variable 'truncate-lines)
+  (setq truncate-lines nil)
+)
+
+(add-hook 'compilation-mode-hook 'lightweight-compilation-hook)
+
+(defun insert-timeofday ()
+   (interactive "*")
+   (insert (format-time-string "---------------- %a, %d %b %y: %I:%M%p")))
+
+; Bright-red TODOs
+ (setq fixme-modes '(c++-mode c-mode emacs-lisp-mode))
+ (make-face 'font-lock-fixme-face)
+ (make-face 'font-lock-note-face)
+ (mapc (lambda (mode)
+	 (font-lock-add-keywords
+	  mode
+	  '(("\\<\\(TODO\\)" 1 'font-lock-fixme-face t)
+            ("\\<\\(NOTE\\)" 1 'font-lock-note-face t))))
+	fixme-modes)
+ (modify-face 'font-lock-fixme-face "Red" nil nil t nil t nil nil)
+ (modify-face 'font-lock-note-face "Dark Green" nil nil t nil t nil nil)
+
+; Accepted file extensions and their appropriate modes
+(setq auto-mode-alist
+      (append
+       '(("\\.cpp$"    . c++-mode)
+         ("\\.hin$"    . c++-mode)
+         ("\\.cin$"    . c++-mode)
+         ("\\.inl$"    . c++-mode)
+         ("\\.rdc$"    . c++-mode)
+         ("\\.h$"    . c++-mode)
+         ("\\.c$"   . c++-mode)
+         ("\\.cc$"   . c++-mode)
+         ("\\.c8$"   . c++-mode)
+         ("\\.txt$" . indented-text-mode)
+         ("\\.emacs$" . emacs-lisp-mode)
+         ("\\.gen$" . gen-mode)
+         ("\\.ms$" . fundamental-mode)
+         ("\\.m$" . objc-mode)
+         ("\\.mm$" . objc-mode)
+         ) auto-mode-alist))
+
+; C++ indentation style
+(defconst lightweight-c-style
+  '((c-electric-pound-behavior   . nil)
+    (c-tab-always-indent         . t)
+    (c-comment-only-line-offset  . 0)
+    (c-hanging-braces-alist      . ((class-open)
+                                    (class-close)
+                                    (defun-open)
+                                    (defun-close)
+                                    (inline-open)
+                                    (inline-close)
+                                    (brace-list-open)
+                                    (brace-list-close)
+                                    (brace-list-intro)
+                                    (brace-list-entry)
+                                    (block-open)
+                                    (block-close)
+                                    (substatement-open)
+                                    (statement-case-open)
+                                    (class-open)))
+    (c-hanging-colons-alist      . ((inher-intro)
+                                    (case-label)
+                                    (label)
+                                    (access-label)
+                                    (access-key)
+                                    (member-init-intro)))
+    (c-cleanup-list              . (scope-operator
+                                    list-close-comma
+                                    defun-close-semi))
+    (c-offsets-alist             . ((arglist-close         .  c-lineup-arglist)
+                                    (label                 . -4)
+                                    (access-label          . -4)
+                                    (substatement-open     .  0)
+                                    (statement-case-intro  .  4)
+                                    (case-label            .  4)
+                                    (block-open            .  0)
+                                    (inline-open           .  0)
+                                    (topmost-intro-cont    .  0)
+                                    (knr-argdecl-intro     . -4)
+                                    (brace-list-open       .  0)
+                                    (brace-list-intro      .  4)))
+    (c-echo-syntactic-information-p . t))
+    "Lightweight Emacs C++ Style")
+
+
+; CC++ mode handling
+(defun lightweight-c-hook ()
+  ; Set my style for the current buffer
+  (c-add-style "Lightweight" lightweight-c-style t)
+  
+  ; 4-space tabs
+  (setq tab-width 4
+        indent-tabs-mode nil)
+
+  ; Additional style stuff
+  (c-set-offset 'member-init-intro '++)
+
+  ; No hungry backspace
+  (c-toggle-auto-hungry-state -1)
+
+  ; Newline indents, semi-colon doesn't
+  (define-key c++-mode-map "\C-m" 'newline-and-indent)
+  (setq c-hanging-semi&comma-criteria '((lambda () 'stop)))
+
+  ; Handle super-tabbify (TAB completes, shift-TAB actually tabs)
+  (setq dabbrev-case-replace t)
+  (setq dabbrev-case-fold-search t)
+  (setq dabbrev-upcase-means-case-search t)
+
+  ; Abbrevation expansion
+  (abbrev-mode 1)
+ 
+  (defun lightweight-find-corresponding-file ()
+    "Find the file that corresponds to this one."
+    (interactive)
+    (setq CorrespondingFileName nil)
+    (setq BaseFileName (file-name-sans-extension buffer-file-name))
+    (if (string-match "\\.c" buffer-file-name)
+       (setq CorrespondingFileName (concat BaseFileName ".h")))
+    (if (string-match "\\.h" buffer-file-name)
+       (if (file-exists-p (concat BaseFileName ".c")) (setq CorrespondingFileName (concat BaseFileName ".c"))
+	   (setq CorrespondingFileName (concat BaseFileName ".cpp"))))
+    (if (string-match "\\.hin" buffer-file-name)
+       (setq CorrespondingFileName (concat BaseFileName ".cin")))
+    (if (string-match "\\.cin" buffer-file-name)
+       (setq CorrespondingFileName (concat BaseFileName ".hin")))
+    (if (string-match "\\.cpp" buffer-file-name)
+       (setq CorrespondingFileName (concat BaseFileName ".h")))
+    (if CorrespondingFileName (find-file CorrespondingFileName)
+       (error "Unable to find a corresponding file")))
+  (defun lightweight-find-corresponding-file-other-window ()
+    "Find the file that corresponds to this one."
+    (interactive)
+    (find-file-other-window buffer-file-name)
+    (lightweight-find-corresponding-file)
+    (other-window -1))
+  (define-key c++-mode-map [f12] 'lightweight-find-corresponding-file)
+  (define-key c++-mode-map [M-f12] 'lightweight-find-corresponding-file-other-window)
+
+  ; devenv.com error parsing
+  (add-to-list 'compilation-error-regexp-alist 'lightweight-devenv)
+  (add-to-list 'compilation-error-regexp-alist-alist '(lightweight-devenv
+   "*\\([0-9]+>\\)?\\(\\(?:[a-zA-Z]:\\)?[^:(\t\n]+\\)(\\([0-9]+\\)) : \\(?:see declaration\\|\\(?:warnin\\(g\\)\\|[a-z ]+\\) C[0-9]+:\\)"
+    2 3 nil (4)))
+)
+
+(defun lightweight-replace-string (FromString ToString)
+  "Replace a string without moving point."
+  (interactive "sReplace: \nsReplace: %s  With: ")
+  (save-excursion
+    (replace-string FromString ToString)
+  ))
+
+(add-hook 'c-mode-common-hook 'lightweight-c-hook)
+
+(defun lightweight-save-buffer ()
+  "Save the buffer after untabifying it."
+  (interactive)
+  (save-excursion
+    (save-restriction
+      (widen)
+      (untabify (point-min) (point-max))))
+  (save-buffer))
+
+; TXT mode handling
+(defun lightweight-text-hook ()
+  ; 4-space tabs
+  (setq tab-width 4
+        indent-tabs-mode nil)
+)
+
+(add-hook 'text-mode-hook 'lightweight-text-hook)
+
+; Window Commands
+(defun w32-restore-frame ()
+    "Restore a minimized frame"
+     (interactive)
+     (w32-send-sys-command 61728))
+
+(defun maximize-frame ()
+    "Maximize the current frame"
+     (interactive)
+     (when lightweight-aquamacs (aquamacs-toggle-full-frame))
+     (when lightweight-win32 (w32-send-sys-command 61488)))
+
+; Navigation
+(defun previous-blank-line ()
+  "Moves to the previous line containing nothing but whitespace."
+  (interactive)
+  (search-backward-regexp "^[ \t]*\n")
+)
+
+(defun next-blank-line ()
+  "Moves to the next line containing nothing but whitespace."
+  (interactive)
+  (forward-line)
+  (search-forward-regexp "^[ \t]*\n")
+  (forward-line -1)
+)
+
+(define-key global-map [C-right] 'forward-word)
+(define-key global-map [C-left] 'backward-word)
+(define-key global-map [C-up] 'previous-blank-line)
+(define-key global-map [C-down] 'next-blank-line)
+(define-key global-map [home] 'beginning-of-line)
+(define-key global-map [end] 'end-of-line)
+(define-key global-map [pgup] 'forward-page)
+(define-key global-map [pgdown] 'backward-page)
+(define-key global-map [C-next] 'scroll-other-window)
+(define-key global-map [C-prior] 'scroll-other-window-down)
+
+; ALT-alternatives
+(defadvice set-mark-command (after no-bloody-t-m-m activate)
+  "Prevent consecutive marks activating bloody `transient-mark-mode'."
+  (if transient-mark-mode (setq transient-mark-mode nil)))
+
+(defadvice mouse-set-region-1 (after no-bloody-t-m-m activate)
+  "Prevent mouse commands activating bloody `transient-mark-mode'."
+  (if transient-mark-mode (setq transient-mark-mode nil))) 
+
+(defun append-as-kill ()
+  "Performs copy-region-as-kill as an append."
+  (interactive)
+  (append-next-kill) 
+  (copy-region-as-kill (mark) (point))
+)
+(define-key global-map [M-up] 'previous-blank-line)
+(define-key global-map [M-down] 'next-blank-line)
+(define-key global-map [M-right] 'forward-word)
+(define-key global-map [M-left] 'backward-word)
+
+(define-key global-map [f9] 'first-error)
+(define-key global-map [f10] 'previous-error)
+(define-key global-map [f11] 'next-error)
+
+(define-key global-map "\en" 'next-error)
+(define-key global-map "\eN" 'previous-error)
+
+; Editing
+(defun lightweight-replace-in-region (old-word new-word)
+  "Perform a replace-string in the current region."
+  (interactive "sReplace: \nsReplace: %s  With: ")
+  (save-excursion (save-restriction
+		    (narrow-to-region (mark) (point))
+		    (beginning-of-buffer)
+		    (replace-string old-word new-word)
+		    ))
+  )
+(define-key global-map "\el" 'lightweight-replace-in-region)
+
+; Compilation
+(setq compilation-context-lines 0)
+(setq compilation-error-regexp-alist
+    (cons '("^\\([0-9]+>\\)?\\(\\(?:[a-zA-Z]:\\)?[^:(\t\n]+\\)(\\([0-9]+\\)) : \\(?:fatal error\\|warnin\\(g\\)\\) C[0-9]+:" 2 3 nil (4))
+     compilation-error-regexp-alist))
+
+(defun lock-compilation-directory ()
+  "The compilation process should NOT hunt for a makefile"
+  (interactive)
+  (setq compilation-directory-locked t)
+  (message "Compilation directory is locked."))
+
+(defun unlock-compilation-directory ()
+  "The compilation process SHOULD hunt for a makefile"
+  (interactive)
+  (setq compilation-directory-locked nil)
+  (message "Compilation directory is roaming."))
+
+; Commands
+(set-variable 'grep-command "grep -irHn ")
+(when lightweight-win32
+    (set-variable 'grep-command "findstr -s -n -i -l "))
+
+; Smooth scroll
+(setq scroll-step 3)
+
+; Clock
+(display-time)
+
+; Startup windowing
+(setq next-line-add-newlines nil)
+(setq-default truncate-lines t)
+(setq truncate-partial-width-windows nil)
+(split-window-horizontally)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(auto-save-default nil)
+ '(auto-save-interval 0)
+ '(auto-save-list-file-prefix nil)
+ '(auto-save-timeout 0)
+ '(auto-show-mode t t)
+ '(delete-auto-save-files nil)
+ '(delete-old-versions (quote other))
+ '(imenu-auto-rescan t)
+ '(imenu-auto-rescan-maxout 500000)
+ '(kept-new-versions 5)
+ '(kept-old-versions 5)
+ '(make-backup-file-name-function (quote ignore))
+ '(make-backup-files nil)
+ '(mouse-wheel-follow-mouse nil)
+ '(mouse-wheel-progressive-speed nil)
+ '(mouse-wheel-scroll-amount (quote (15)))
+ '(version-control nil))
+
+(defun lightweight-never-split-a-window
+    "Never, ever split a window.  Why would anyone EVER want you to do that??"
+    nil)
+(setq split-window-preferred-function 'lightweight-never-split-a-window)
+
+(add-to-list 'default-frame-alist '(font . "Liberation Mono-11.5"))
+(set-face-attribute 'default t :font "Liberation Mono-11.5")
+(set-face-attribute 'font-lock-builtin-face nil :foreground "#DAB98F")
+(set-face-attribute 'font-lock-comment-face nil :foreground "gray50")
+(set-face-attribute 'font-lock-constant-face nil :foreground "olive drab")
+(set-face-attribute 'font-lock-doc-face nil :foreground "gray50")
+(set-face-attribute 'font-lock-function-name-face nil :foreground "burlywood3")
+(set-face-attribute 'font-lock-keyword-face nil :foreground "DarkGoldenrod3")
+(set-face-attribute 'font-lock-string-face nil :foreground "olive drab")
+(set-face-attribute 'font-lock-type-face nil :foreground "burlywood3")
+(set-face-attribute 'font-lock-variable-name-face nil :foreground "burlywood3")
+
+(defun post-load-stuff ()
+  (interactive)
+  (menu-bar-mode -1)
+  (maximize-frame)
+  (set-foreground-color "burlywood3")
+  (set-background-color "#161616")
+  (set-cursor-color "#40FF40")
+)
+(add-hook 'window-setup-hook 'post-load-stuff t)
