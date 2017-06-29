e6e04574fc13ff0930bf51845d3a0a81dbb36119
yliangsiew <yliangsiew@blizzard.com>
2017-06-29 10:36:03 -0700

    changed theme to zenburn

--- lightweight_config.el
+++ lightweight_config.el
@@ -14,6 +14,8 @@
 ; Start maximized
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 
+(setq inhibit-startup-screen t)
+
 (setq compilation-directory-locked nil)
 (scroll-bar-mode -1)
 (setq shift-select-mode nil)
@@ -392,22 +394,13 @@
 
 (add-to-list 'default-frame-alist '(font . "Liberation Mono-11.5"))
 (set-face-attribute 'default t :font "Liberation Mono-11.5")
-(set-face-attribute 'font-lock-builtin-face nil :foreground "#DAB98F")
-(set-face-attribute 'font-lock-comment-face nil :foreground "gray50")
-(set-face-attribute 'font-lock-constant-face nil :foreground "olive drab")
-(set-face-attribute 'font-lock-doc-face nil :foreground "gray50")
-(set-face-attribute 'font-lock-function-name-face nil :foreground "burlywood3")
-(set-face-attribute 'font-lock-keyword-face nil :foreground "DarkGoldenrod3")
-(set-face-attribute 'font-lock-string-face nil :foreground "olive drab")
-(set-face-attribute 'font-lock-type-face nil :foreground "burlywood3")
-(set-face-attribute 'font-lock-variable-name-face nil :foreground "burlywood3")
-
+(add-to-list 'custom-theme-load-path
+             (file-name-as-directory "~/Git/lightweight-emacs/themes"))
 (defun post-load-stuff ()
   (interactive)
   (menu-bar-mode -1)
   (maximize-frame)
-  (set-foreground-color "burlywood3")
-  (set-background-color "#161616")
+  (load-theme 'zenburn t)
   (set-cursor-color "#40FF40")
 )
 (add-hook 'window-setup-hook 'post-load-stuff t)
