9c60cdac87545e52e52c16358cf55f34ad57bc93
yliangsiew <yliangsiew@blizzard.com>
2017-06-30 10:24:59 -0700

    added auto highlighting of all matching symbols during search

--- lightweight_config.el
+++ lightweight_config.el
@@ -10,6 +10,12 @@
 ;        ))
 (yas-global-mode 1)
 
+(require 'highlight-symbol)
+(global-set-key [(control f3)] 'highlight-symbol)
+(global-set-key [f3] 'highlight-symbol-next)
+(global-set-key [(shift f3)] 'highlight-symbol-prev)
+(global-set-key [(meta f3)] 'highlight-symbol-query-replace)
+
 ; Vertical command minibuffer
 (require 'ido)
 (require 'ido-vertical-mode)
