698b283862e1e42faa9627648b273bf3f219ec34
Siew Yi Liang <admin@sonictk.com>
2018-04-25 00:22:17 -0700

    added rescan-symbols-in-buffer function for manual rescanning of buffers

--- lightweight_config.el
+++ lightweight_config.el
@@ -172,6 +172,11 @@
 (global-set-key (kbd "C-c h i") 'ido-goto-symbol)
 (global-set-key (kbd "<C-f12>") 'ido-goto-symbol)
 
+; Allow for manual-rescanning of buffers
+ (defun rescan-symbols-in-buffer()
+   (interactive)
+   (imenu--menubar-select imenu--rescan-item))
+
 ; Do not save semanticdb file to user home emacs directory
 (setq semanticdb-default-save-directory "~/Git/lightweight-emacs/semantic-cache")
 
