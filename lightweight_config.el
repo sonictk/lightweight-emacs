129e6fe18005176564a20082ea5ba3aa4928be6c
Siew Yi Liang <admin@sonictk.com>
2019-02-02 16:17:59 -0800

    added support for Markdeep files

--- lightweight_config.el
+++ lightweight_config.el
@@ -965,6 +965,7 @@ current buffer's, reload dir-locals."
    "Major mode for editing Markdown files" t)
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md.html\\'" . markdown-mode))
 
 (autoload 'gfm-mode "markdown-mode"
    "Major mode for editing GitHub Flavored Markdown files" t)
