acb8d44d0c8f8890c2d0b3d02d1d085e251ef0b8
yliangsiew <yliangsiew@blizzard.com>
2019-05-31 16:09:08 -0700

    revert change for gdb source window since it affects normal workflow as well

--- lightweight_config.el
+++ lightweight_config.el
@@ -594,33 +594,33 @@ current buffer's, reload dir-locals."
 (global-set-key (kbd "<f7>") 'gud-step)
 
 ; GDB control which windows new buffers are displayed in.
-(add-to-list 'display-buffer-alist
-         (cons 'cdb-source-code-buffer-p
-           (cons 'display-source-code-buffer nil)))
-
-(defun cdb-source-code-buffer-p (bufName action)
-  "Return whether BUFNAME is a source code buffer."
-  (let ((buf (get-buffer bufName)))
-    (and buf
-     (with-current-buffer buf
-       (derived-mode-p buf 'c++-mode 'c-mode 'csharp-mode 'nxml-mode)))))
-
-(defun display-source-code-buffer (sourceBuf alist)
-  "Find a window with source code and set sourceBuf inside it."
-  (let* ((curbuf (current-buffer))
-     (wincurbuf (get-buffer-window curbuf))
-     (win (if (and wincurbuf
-               (derived-mode-p sourceBuf 'c++-mode 'c-mode 'nxml-mode)
-               (derived-mode-p (current-buffer) 'c++-mode 'c-mode 'nxml-mode))
-          wincurbuf
-        (get-window-with-predicate
-         (lambda (window)
-           (let ((bufName (buffer-name (window-buffer window))))
-             (or (cdb-source-code-buffer-p bufName nil)
-             (assoc bufName display-buffer-alist)
-             ))))))) ;; derived-mode-p doesn't work inside this, don't know why...
-    (set-window-buffer win sourceBuf)
-    win))
+;(add-to-list 'display-buffer-alist
+;         (cons 'cdb-source-code-buffer-p
+;           (cons 'display-source-code-buffer nil)))
+;
+;(defun cdb-source-code-buffer-p (bufName action)
+;  "Return whether BUFNAME is a source code buffer."
+;  (let ((buf (get-buffer bufName)))
+;    (and buf
+;     (with-current-buffer buf
+;       (derived-mode-p buf 'c++-mode 'c-mode 'csharp-mode 'nxml-mode)))))
+;
+;(defun display-source-code-buffer (sourceBuf alist)
+;  "Find a window with source code and set sourceBuf inside it."
+;  (let* ((curbuf (current-buffer))
+;     (wincurbuf (get-buffer-window curbuf))
+;     (win (if (and wincurbuf
+;               (derived-mode-p sourceBuf 'c++-mode 'c-mode 'nxml-mode)
+;               (derived-mode-p (current-buffer) 'c++-mode 'c-mode 'nxml-mode))
+;          wincurbuf
+;        (get-window-with-predicate
+;         (lambda (window)
+;           (let ((bufName (buffer-name (window-buffer window))))
+;             (or (cdb-source-code-buffer-p bufName nil)
+;             (assoc bufName display-buffer-alist)
+;             ))))))) ;; derived-mode-p doesn't work inside this, don't know why...
+;    (set-window-buffer win sourceBuf)
+;    win))
 
 ; GDB Restore windows layout after debugging and also nicer default layout
 (setq gdb-many-windows nil)
