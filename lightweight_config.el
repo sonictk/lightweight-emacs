d2c4d03d2491debc30ad9b345e54f9f2086c9ce9
Siew Yi Liang <admin@sonictk.com>
2020-03-11 18:10:11 -0700

    changed keybinding for highlight symbol to be easier to execute

--- lightweight_config.el
+++ lightweight_config.el
@@ -30,7 +30,7 @@
 
 (require 'highlight-symbol)
 (global-set-key [(control f3)] 'highlight-symbol)
-(global-set-key (kbd "C-S-M-f") 'highlight-symbol)
+(global-set-key (kbd "C-c h h") 'highlight-symbol)
 (global-set-key [f3] 'highlight-symbol-next)
 (global-set-key [(shift f3)] 'highlight-symbol-prev)
 (global-set-key [(meta f3)] 'highlight-symbol-query-replace)
