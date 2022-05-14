0abb43f2142855db99ce45d932edc07360f1be8f
Siew Yi Liang <yiliang.siew@epicgames.com>
2022-05-13 17:10:48 -0700

    saner eglot defaults

--- lightweight_config.el
+++ lightweight_config.el
@@ -147,6 +147,11 @@ If the input is empty, select the previous history element instead."
 
 (setq eglot-autoshutdown 1)
 (setq eglot-autoreconnect nil)
+(setq eglot-connect-timeout 0)
+(setq eglot-sync-connect t)
+(setq eglot-confirm-server-initiated-edits nil)
+(setq eglot-strict-mode nil)
+(setq eglot-extend-to-xref 1)
 
 ; Don't want the eldoc box showing everywhere, have a global bind for it
 (add-hook 'eglot--managed-mode-hook #'eldoc-box-hover-mode t)
