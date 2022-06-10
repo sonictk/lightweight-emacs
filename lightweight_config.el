147f0a1edcbdea839242b7d6be774a27f2b2f887
Siew Yi Liang <yiliang.siew@epicgames.com>
2022-06-10 16:05:32 -0700

    turn on auto reconnect for now

--- lightweight_config.el
+++ lightweight_config.el
@@ -154,7 +154,7 @@ If the input is empty, select the previous history element instead."
 ; Python already works OOTB
 
 (setq eglot-autoshutdown t)
-(setq eglot-autoreconnect nil)
+(setq eglot-autoreconnect t)
 (setq eglot-connect-timeout 20)
 (setq eglot-extend-to-xref t)
 
