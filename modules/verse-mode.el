3bf9f97571df51a1c38e1a76846177cd90e549c3
Siew Yi Liang <yiliang.siew@epicgames.com>
2022-09-11 22:24:37 -0700

    fixed the syntax highlighting to work better for comments all round and also fixed punctuation for more operators

--- modules/verse-mode.el
+++ modules/verse-mode.el
@@ -128,37 +128,36 @@
   `((
      ; Highlight special Verse characters TODO but this breaks other syntax highlighting. Maybe better to define these in the syntax table. Somehow.
      ; ! | ? | ^ | : | := | ; | @ | = | < | > | + | - | * | /
-     ("!\\|?\\|^\\|:\\|:=\\|;\\|@\\|=\\|<\\|>\\|+\\|-\\|*\\|/" . font-lock-keyword-face)
+     ; ("!\\|?\\|^\\|:\\|:=\\|;\\|@\\|=\\|<\\|>\\|+\\|-\\|*\\|/" . font-lock-keyword-face)
      ; For character literals
-     ("'[[:alnum:]]+'" . font-lock-string-face)
      ( ,(regexp-opt verse-keywords 'words) . font-lock-keyword-face)
      ( ,(regexp-opt verse-constants 'words) . font-lock-constant-face)
      ( ,(regexp-opt verse-builtin-types 'words) . font-lock-type-face)
      ( ,(regexp-opt verse-builtin-procs 'words) . font-lock-builtin-face)
      ; Single-line comment syntax highlighting
-     ("#.+" . font-lock-comment-face)
+     ; ("#.+" . font-lock-comment-face)
      ; Actual regex for indcmt
      ; <#>\n([ \t].+\n)+
      ; Block comment regex TODO: This won't work well in all cases. Should look at: http://xahlee.info/emacs/emacs_manual/elisp/Multiline-Font-Lock.html#Multiline-Font-Lock
      ; <#([^#]*)#>
-     ; ("<#>\n\\([ \t].+\n\\)+\\|#.+\\|<#\\([^#]*\\)#>" 0 font-lock-comment-face t)
+     ("<#>\n\\([ \t].+\n\\)+\\|<#\\([^#]*\\)#>" 0 font-lock-comment-face t)
      ))
 )
 
 ; TODO: Support all Verse comment styles correctly.
 (defvar verse-mode-syntax-table
   (let ((st (make-syntax-table)))
-    ; Syntax for curly braces and parentheses.
+    ; Syntax for curly braces, parentheses and brackets.
     (modify-syntax-entry ?{ "(}" st)
     (modify-syntax-entry ?} "){" st)
     (modify-syntax-entry ?\( "()" st)
     (modify-syntax-entry ?\) "))" st)
-    ; Syntax for brackets.
     (modify-syntax-entry ?\[ "(]" st)
     (modify-syntax-entry ?\] ")[" st)
-    ; Syntax for carets. But this interferes with block comments!
-    ; (modify-syntax-entry ?< "(>" st)
-    ; (modify-syntax-entry ?> ")<" st)
+    ; Syntax for carets. (That are used in attributes.) 
+    ; We can't really use these because it's part of math operators as well.
+    (modify-syntax-entry ?< "(<" st)
+    (modify-syntax-entry ?> ")>" st)
 
     ;; - and _ are word constituents
     (modify-syntax-entry ?_ "w" st)
@@ -166,11 +165,11 @@
 
     ;; String/char delimiters
     ;; ' is a char delimiter; however because we can't put comments in the syntax table we can't put this in either.
-    ; (modify-syntax-entry ?\' "\"" st)
+    (modify-syntax-entry ?\' "\"" st)
     ;; " is a string delimiter too
     (modify-syntax-entry ?\" "\"" st)
 
-    ; NOTE Supporting all 3 comment styles of Verse in the syntax table alone...is difficult.
+    ; NOTE Supporting all 3 comment styles of Verse in the syntax table alone...is difficult. Because they share the same character, but in different ways.
     ; So we do it as a regex instead that captures all 3 types. Cause otherwise <# #> and <#> interfere with each other.
     ; Block comment of style <# ... #>
     ; (modify-syntax-entry ?< ". 1" st)
@@ -178,8 +177,8 @@
     ; (modify-syntax-entry ?> ". 4" st)
 
     ; Line comments of style # ...
-    ; (modify-syntax-entry ?# "<" st)
-    ; (modify-syntax-entry ?\n ">" st)
+    (modify-syntax-entry ?# "<" st)
+    (modify-syntax-entry ?\n ">" st)
 
     ; '==' as punctuation
     (modify-syntax-entry ?= ".")
@@ -195,10 +194,22 @@
     (modify-syntax-entry ?& ".")
     (modify-syntax-entry ?| ".")
     (modify-syntax-entry ?: ".")
+    ; We can't re-use this as punctuation since it interferes with the syntax table entry for comments.
+    ; (modify-syntax-entry ?< ".")
+    ; (modify-syntax-entry ?> ".")
 
-    ; pointers and optionals as punctuation
+    ; Other reserved symbols in Verse as punctuation
+    (modify-syntax-entry ?@ ".")
+    (modify-syntax-entry ?: ".")
+    (modify-syntax-entry ?\; ".")
+
+    ; pointers and optionals as punctuation; this will be superceded by use of `var`/`set`
     (modify-syntax-entry ?? ".")
     (modify-syntax-entry ?^ ".")
+
+    ; whitespace syntax class
+    (modify-syntax-entry ?\s " ")
+    (modify-syntax-entry ?\t " ")
     st
   )
 )
@@ -230,6 +241,11 @@
   "VERSE mode is a major mode for editing VERSE files"
   :syntax-table verse-mode-syntax-table
   ;:abbrev-table verse-mode-abbrev-table
+
+  ; Because of how Verse comments work, we cannot have all the rules set in a single 
+  ; syntax table together; thus we need to use a regex, and thus we need multiline font-lock
+  ; support.
+  (setq-local font-lock-multiline t)
 
   (setq-local font-lock-defaults verse-font-lock-defaults)
   (when verse-tab-width
