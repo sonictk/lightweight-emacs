40df44b096b0d844a78443edbd5f8cd165b03b8d
Siew Yi Liang <yiliang.siew@epicgames.com>
2022-10-14 10:35:14 -0700

    added more convenience p4 commands

--- modules/p4-extensions.el
+++ modules/p4-extensions.el
@@ -20,4 +20,31 @@
      (append (list "-p" "-r" "-c" (p4-completing-read 'shelved "Changelist: ")))))
   (p4-call-command "shelve" args :mode 'p4-basic-list-mode))
 
+(defp4cmd p4-shelve-discard-files (&rest args)
+  "shelve"
+  "Discards shelved files (or a stream spec) for a pending changelist in the depot."
+  (interactive
+   (if current-prefix-arg
+       (p4-read-args "p4 shelve" "" 'shelved)
+     (append (list "-d" "-c" (p4-completing-read 'shelved "Changelist: ")))))
+  (p4-call-command "shelve" args :mode 'p4-basic-list-mode))
+
+(defp4cmd p4-change-delete (&rest args)
+  "change"
+  "Delete the changelist. This is only allowed if the pending changelist has no files or pending fixes."
+  (interactive
+   (if current-prefix-arg
+       (p4-read-args "p4 change" "" 'shelved)
+     (append (list "-d" (p4-completing-read 'shelved "Changelist: ")))))
+  (p4-call-command "change" args :mode 'p4-basic-list-mode))
+
+(defp4cmd p4-revert-changelist (&rest args)
+  "revert"
+  "Reverts only those files in the specified changelist."
+  (interactive
+   (if current-prefix-arg
+       (p4-read-args "p4 revert" "" 'shelved)
+     (append (list "-c" (p4-completing-read 'shelved "Changelist: ")))))
+  (p4-call-command "revert" args :mode 'p4-basic-list-mode))
+
 (provide 'p4-extensions)
