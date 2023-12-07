10c3d448b117ea1ec1abbd91395699c3e45d0d38
Siew Yi Liang <yiliang.siew@epicgames.com>
2023-12-07 15:57:28 -0800

    made command async and prints output to a temp buffer as well

--- modules/epic-games-internal.el
+++ modules/epic-games-internal.el
@@ -8,11 +8,23 @@
          (p4client (getenv "P4CLIENT"))
          (localappdata (getenv "LOCALAPPDATA"))
          (root-dir (string-trim-right (shell-command-to-string "p4 -F %clientRoot% -ztag info")))
-         (cl (p4-completing-read 'shelved "Changelist: ")))
+         (cl (p4-completing-read 'shelved "Changelist: "))
+         (buffer-name "*Epic SubmitTool*"))
     (let ((executable (concat localappdata "\\UnrealGameSync\\Tools\\SubmitTool\\Current\\Windows\\Engine\\Binaries\\Win64\\SubmitTool.exe")))
-      (call-process executable nil 0 nil
-                    "-server" p4port
-                    "-user" p4user
-                    "-client" p4client
-                    "-root-dir" root-dir
-                    "-cl" cl))))
+      (with-current-buffer (get-buffer-create buffer-name)
+        (setq buffer-read-only t)
+        (display-buffer (current-buffer))
+        (let ((proc (start-process "Epic-SubmitTool" buffer-name executable
+                                   "-server" p4port
+                                   "-user" p4user
+                                   "-client" p4client
+                                   "-root-dir" root-dir
+                                   "-cl" cl)))
+          (set-process-sentinel proc
+                                (lambda (process event)
+                                  (when (string= event "finished\n")
+                                    (setq buffer-read-only nil)
+                                    (insert "\n[Process completed]\n")
+                                    (setq buffer-read-only t))))))))
+  (with-current-buffer "*Epic SubmitTool*"
+    (local-set-key "q" (lambda () (interactive) (quit-window t)))))
